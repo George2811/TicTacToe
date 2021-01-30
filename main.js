f43527b814885948ee30fdc03be18499fcd14b6a
George2811 <sanchez28112001@gmail.com>
2021-01-29 22:44:41 -0500

    Basic Game Finished

--- main.js
+++ main.js
@@ -1,36 +1,90 @@
 
-var turn = true;
+var turn = true, player = true;
 /*Sections*/
 const $data = document.querySelector(".data");
 const $game = document.querySelector(".game");
+const $winner = document.querySelector(".winner");
+const $playerWin = document.querySelector(".tittle-winner");
+const $pcWin = document.querySelector(".pc");
 /*DOM Declare*/
 const $name = document.querySelector(".name-data");
 const $noname = document.querySelector(".no-name");
 const $buttonReady = document.querySelector(".button-data");
 const $nameinGame = document.querySelector(".human");
 const $boxes = document.querySelectorAll(".box");
+const $buttonPlayAgain = document.querySelector(".button-winner");
+
 
 
 function catchName() {
-    if($name.value!="")
-    {
+    if ($name.value != "") {
         $nameinGame.setAttribute("value", $name.value);
         $data.style.display = "none";
         $game.style.display = "flex";
     }
-    else{
+    else {
         $noname.style.display = "block";
     }
-
 }
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
+function winner() {
+
+    for (let i = 0; i < $boxes.length; i++) {
+        if (i == 0 || i == 3 || i == 6) {
+            if ($boxes[i].value) {
+                if ($boxes[i].value === $boxes[i + 1].value && $boxes[i + 1].value === $boxes[i + 2].value){
+                    $boxes[i].value=="x"?player=true:player=false;
+                    console.log(player);
+                    return true;
+                }
+            }
+        }
+        if (i == 0 || i == 1 || i == 2) {
+            if ($boxes[i].value) {
+                if ($boxes[i].value === $boxes[i + 3].value && $boxes[i + 3].value === $boxes[i + 6].value){
+                    $boxes[i].value=="x"?player=true:player=false;
+                    console.log(player);
+                    return true;
+                }
+            }
+        }
+        if (i == 0) {
+            if ($boxes[i].value) {
+                if ($boxes[i].value === $boxes[i + 4].value && $boxes[i + 4].value === $boxes[i + 8].value) {
+                    $boxes[i].value=="x"?player=true:player=false;
+                    console.log(player);
+                    return true;
+                }
+            }
+        }
+        if (i == 2) {
+            if ($boxes[i].value) {
+                if ($boxes[i].value === $boxes[i + 2].value && $boxes[i + 2].value === $boxes[i + 4].value) {
+                    $boxes[i].value=="x"?player=true:player=false;
+                    console.log(player);
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+function whoWin(name){
+    if(name){
+        $playerWin.style.display = "block";
+    }
+    else{
+        $pcWin.style.display = "block";        
+    }
+}
+
 function gameplay() {
     while (turn) {
-        for (let i = 0; i < $boxes.length; i++) {
-            var rdNum = getRandomInt(0,9);
+        for (let i = 0; i < $boxes.length + 10; i++) {
+            let rdNum = getRandomInt(0, 9);
             console.log(rdNum);
             if ($boxes[rdNum].value == "x" || $boxes[rdNum].value == "o") {
             }
@@ -38,12 +92,13 @@ function gameplay() {
                 $boxes[rdNum].setAttribute("value", "o");
                 return;
             }
-           //llega hasta aqui;            
+            //llega hasta aqui;            
         }
         turn = false;
     }
     turn = true;
 }
+
 
 
 $buttonReady.addEventListener("click", function (e) {
@@ -53,10 +108,19 @@ $buttonReady.addEventListener("click", function (e) {
 
 $boxes.forEach((e, index) => {
     e.addEventListener("click", function (e) {
-        if($boxes[index].value == ""){
+        if ($boxes[index].value == "") {
             $boxes[index].setAttribute("value", "x");
             gameplay();
+            if (winner()) {
+                console.log("Hay ganador");
+                $game.style.display="none";
+                $winner.style.display="flex";
+                whoWin(player);
+            }
             console.log($boxes[index]);
         }
     });
+});
+$buttonPlayAgain.addEventListener("click",e=>{
+    location.reload();
 });
